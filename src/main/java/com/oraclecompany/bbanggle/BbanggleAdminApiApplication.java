97d364e83855379b5b54c2a7f1b1f061fa93079b
sangmin-j <sangmin@oracleadinfo.com>
2022-11-25 16:50:26 +0900

    Initial commit

---
+++ src/main/java/com/oraclecompany/bbanggle/BbanggleAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.oraclecompany.bbanggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbanggleAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbanggleAdminApiApplication.class, args);
+    }
+
+}
